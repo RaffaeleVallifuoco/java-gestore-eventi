3b977ebb8e1839ce5fa142fbdba5f4ee291a4703
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-05-21 17:40:57 +0200

    ADD : app implementation : scelta evento

--- Gestore Eventi/src/it/java/gestoreEventi/App.java
+++ Gestore Eventi/src/it/java/gestoreEventi/App.java
@@ -7,26 +7,93 @@ public class App {
         System.out.println("\n - TICKETRAFFO - \n");
 
         Scanner scan = new Scanner(System.in);
+        boolean inputValido = false;
 
         System.out.println("\n A quale evento sei interessato ? \n");
-        System.out.println(
-                "\n Eventi disponibili :    COLDPLAY concerto evento Milano S.Siro(07/07/2025)   |    E FUORI NEVICA prima teatrale (04/08/1999) \n");
-        System.out.println("\n ! INSERIRE SOLO NOME EVENTO (coldplay / efuorinevica) SENZA SPAZI ! \n");
 
-        String choice = scan.nextLine();
+        while (!inputValido) {
+            System.out.println(
+                    "\n Eventi disponibili :    COLDPLAY concerto evento Milano S.Siro(07/07/2025)   |    E FUORI NEVICA prima teatrale (04/08/1999) \n");
+            System.out.println("\n ! INSERIRE SOLO NOME EVENTO (coldplay / efuorinevica) SENZA SPAZI ! \n");
 
-        if (choice.equalsIgnoreCase("")) {
+            String choice = scan.nextLine();
 
-            Evento event = new ConcertoPrato();
+            // istanzio evento generico che verrà specificato in seguito
 
-            System.out.printf("\n Stai prenotando : %s ", event.toString());
+            Evento newEvent = null;
 
-            System.out.print("\n Quanti posti vuoi orenotare  per l'evento ? ");
-            int postiDaPrenotare = scan.nextInt();
+            // controllo validità imput utente
 
-            event.prenota(postiDaPrenotare);
+            if (choice.equalsIgnoreCase("coldplay") || choice.equalsIgnoreCase("efuorinevica")) {
+                inputValido = true;
 
+                switch (choice.toLowerCase()) {
+                    case "efuorinevica":
+
+                        newEvent = new SpettacoloTeatrale();
+
+                        break;
+
+                    case "coldplay":
+
+                        System.out.println("\n A che posto sei interessato ? (PRATO  |  I ANELLO  |  TRIBUNA)");
+                        String ticketChoice = scan.nextLine();
+
+                        switch (ticketChoice.toLowerCase()) {
+                            case "prato":
+
+                                newEvent = new ConcertoPrato();
+
+                                break;
+
+                            case "ianello":
+
+                                newEvent = new ConcertoIAnello();
+
+                                break;
+
+                            case "tribuna":
+
+                                newEvent = new ConcertoTribuna();
+
+                                break;
+
+                            default:
+                                System.out.println("\n Scelta non corretta \n");
+                                break;
+                        }
+
+                        break;
+
+                    default:
+                        System.out.println("\n Scelta non corretta \n");
+                        break;
+                }
+
+                if (newEvent != null) {
+
+                    System.out.printf("\n Hai scelto : %s ", newEvent.toString());
+
+                }
+
+            } else {
+                System.out.println("Errore: l'input non è valido. Riprova.");
+            }
         }
-
     }
+
+    // if (choice.equalsIgnoreCase("")) {
+
+    // Evento event = new ConcertoPrato();
+
+    // System.out.printf("\n Stai prenotando : %s ", event.toString());
+
+    // System.out.print("\n Quanti posti vuoi orenotare per l'evento ? ");
+    // int postiDaPrenotare = scan.nextInt();
+
+    // event.prenota(postiDaPrenotare);
+
+    // }
+
+    // }
 }
